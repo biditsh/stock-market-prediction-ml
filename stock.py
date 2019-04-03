06b31ae4dc8fbcf88903203f610fbd820ef8a9d1
Bidit Sharma <sharmabidit@gmail.com>
2019-04-03 14:56:26 -0500

    asks for Ticket

--- stock.py
+++ stock.py
@@ -7,9 +7,16 @@ import urllib.request, json
 import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
+from pandas_datareader import DataReader
 
 # df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
-df = pd.read_csv('yahoo.txt',delimiter=',',usecols=['Date','High','Low','Open','Close', 'Volume', 'Adj Close'])
+
+tkr = input("Enter Ticker :")
+
+data = DataReader(tkr,  'yahoo', dt.datetime(2008, 1, 1), dt.datetime(2019, 4, 3))
+data.to_csv(tkr+".txt", sep=',')
+
+df = pd.read_csv(tkr+'.txt',delimiter=',',usecols=['Date','High','Low','Open','Close', 'Volume', 'Adj Close'])
 print('Loaded data from the Kaggle')
 
 df = df.sort_values('Date')
@@ -24,7 +31,7 @@ print(df.head())
 # plt.ylabel('Mid Price',fontsize=18)
 # plt.show()
 
-#Breaking the data into train and test and normalizing the data
+# Breaking the data into train and test and normalizing the data
 
 # First calculate the mid prices from the highest and lowest 
 high_prices = df.loc[:,'High'].values
@@ -52,19 +59,19 @@ test_data = mid_prices[div:]
 #     scaler.fit(train_data[di:di+smoothing_window_size,:])
 #     train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
 
-# You normalize the last bit of remaining data 
+# # You normalize the last bit of remaining data 
 # scaler.fit(train_data[di+smoothing_window_size:,:])
 # train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
 
-# Reshape both train and test data
+# # Reshape both train and test data
 # train_data = train_data.reshape(-1)
 
-# Normalize test data
+# # Normalize test data
 # test_data = scaler.transform(test_data).reshape(-1)
 
 
-# Now perform exponential moving average smoothing
-# So the data will have a smoother curve than the original ragged data
+# # Now perform exponential moving average smoothing
+# # So the data will have a smoother curve than the original ragged data
 # EMA = 0.0
 # gamma = 0.1
 # for ti in range(div):
